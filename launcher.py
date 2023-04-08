b663d4ac3b67d45d2cddf066ddee74a5982add32
Matheus Lougen <49339085+theSystemFall@users.noreply.github.com>
2023-04-08 11:53:35 -0300

    refactor: remove useless code and update typing

--- launcher.py
+++ launcher.py
@@ -1,54 +1,48 @@
 #!/usr/bin/env python
 
-import logging
 import asyncio
+from typing import NoReturn
 
-import discord
-
-from systembot import Client
+import systembot
 from systembot.modules import logger
 from systembot.modules import Config
 
 
-def setup_logging():
-    formatter = logger.Formatter('%(asctime)s %(name)s %(levelname)-4s [%(filename)s:%(lineno)d] %(message)s')
-    colored_formatter = logger.ColoredFormatter(
-        '%(light_green)s%(asctime)s %(red)s%(name)s %(log_color)s%(levelname)s %(yellow)s[%(filename)s:%(lineno)d]%(reset)s %(message)s'
-        )
-    logger.create('main',
-                  level=logger.DEBUG,
-                  handlers=[logger.StreamHandler(formatter=colored_formatter),
-                            logger.FileHandler('./data/logs/main.log', formatter=formatter)])
-    logger.create('extensions',
-                  level=logger.DEBUG,
-                  handlers=[logger.StreamHandler(formatter=colored_formatter),
-                            logger.FileHandler('./data/logs/extensions.log', formatter=formatter)])
+def setup_logging() -> None:
+    """Setup discord and wavelink loggers"""
+    logger.create('wavelink',
+                  level=logger.WARNING,
+                  handlers=[logger.StreamHandler(),
+                            logger.FileHandler('./data/logs/wavelink.log')])
+    logger.create('wavelink.websocket',
+                  level=logger.WARNING,
+                  handlers=[logger.StreamHandler(),
+                            logger.FileHandler('./data/logs/wavelink.log')])
     logger.create('discord',
                   level=logger.WARNING,
-                  handlers=[logger.StreamHandler(formatter=colored_formatter),
-                            logger.FileHandler('./data/logs/discord.log', formatter=formatter)])
+                  handlers=[logger.StreamHandler(),
+                            logger.FileHandler('./data/logs/discord.log')])
     logger.create('discord.https',
                   level=logger.WARNING,
-                  handlers=[logger.StreamHandler(formatter=colored_formatter),
-                            logger.FileHandler('./data/logs/discord.https.log', formatter=formatter)])
-    log = logger.get('main')
-    return log
+                  handlers=[logger.StreamHandler(),
+                            logger.FileHandler('./data/logs/discord.https.log')])
 
 
-def main():
+def main() -> None:
     initial_extensions = (
-        'systembot.extensions.developer_commands',
-        'systembot.extensions.admin_commands'
-    )
+        'commands.developer_commands',
+        'commands.user_commands',
+        'music.commands',
+        'music.events',
+        'music.exceptions',
+        )
 
-    log = setup_logging()
-    client = Client(config=Config(path='./data/config.yaml'),
-                    initial_extensions=initial_extensions)
-
+    setup_logging()
+    client = systembot.Client(config=Config(path='./data/config.yaml'), initial_extensions=initial_extensions)
     asyncio.run(run_bot(client))
 
 
-async def run_bot(client):
+async def run_bot(client: systembot.Client) -> NoReturn:
     async with client:
         await client.start(client.config.token)
 
