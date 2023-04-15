3d4d40dc5959728ebd164199699bf8176a11f7ec
theSystemFall <matheus.lougen@gmail.com>
2023-04-15 12:01:37 -0300

    refactor: move files to better places

--- systembot/extensions/checks.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord import app_commands
-
-def is_owner():
-    def predicate(interaction: discord.Interaction) -> bool:
-        return interaction.user.id == 346244061872652289
-    return app_commands.check(predicate)
