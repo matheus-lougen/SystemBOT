22fd860efaf78bb7912359c70d67724157ad246a
theSystemFall <matheus.lougen@gmail.com>
2023-04-15 17:45:41 -0300

    refactor: clean logging wrapper code
    removed unused classes, added docstrings and typing

--- systembot/modules/logger/__init__.py
+++ systembot/modules/logger/__init__.py
@@ -1,9 +1,33 @@
-import logging
-import functools
-from typing import Optional, List
+"""A logging library wrapper to make logging easy and simple.
 
-from .handlers import StreamHandler, FileHandler
-from .formatters import Formatter, ColoredFormatter
+MIT License
+
+Copyright (c) 2023 Matheus Lougen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import logging
+
+from typing import List, Union, Optional
+
+from .handlers import FileHandler, StreamHandler
 
 DEBUG = 10
 INFO = 20
@@ -12,17 +36,24 @@ ERROR = 40
 CRITICAL = 50
 
 
-def create(name: str, level=DEBUG, handlers: list = None) -> None:
+def create(
+    name: str, level: int = DEBUG, handlers: List[Union[FileHandler, StreamHandler]] = None
+) -> Optional[logging.Logger]:
+    """Create a logger instance
+    Returns a logger instance ready to use, defaults the handlers formatter to default values
+    """
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
 
-    if isinstance(handlers, list):
-        for handler in handlers:
-            log.addHandler(handler)
-            handler.setLevel(level)
-        log.debug(f'New logger sucessfully created [{name}]')
-    else:
-        raise TypeError(f'Handlers has to be of type List not {type(handlers)}')
+    for handler in handlers:
+        log.addHandler(handler)
+        handler.setLevel(level)
+
+    return log
+
 
 def get(name: str) -> logging.Logger:
+    """Get a logger instance
+    Returns the logger instance
+    """
     return logging.getLogger(name)
--- systembot/modules/logger/handlers.py
+++ systembot/modules/logger/handlers.py
@@ -1,16 +1,68 @@
+"""Handlers with default formatters.
+
+MIT License
+
+Copyright (c) 2023 Matheus Lougen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import logging
+from typing import Optional, Any
+
+import colorlog
 
 
 class StreamHandler(logging.StreamHandler):
-    def __init__(self, formatter=None):
+    """Represents a StreamHandler.
+    Subclasses logging.StreamHandler
+    Logs directly to the console output, defaults to a predefined colorlog colored formatter
+    """
+
+    def __init__(self, formatter: Optional[Any] = None) -> None:
         super().__init__()
-        if isinstance(formatter, type(None)):
-            raise TypeError('Logger formatter cannot be of type None')
+        if formatter is None:
+            colors = {
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red',
+            }
+            formatter = colorlog.ColoredFormatter(
+                '%(light_green)s%(asctime)s %(red)s%(name)s %(log_color)s%(levelname)s %(yellow)s[%(filename)s:%(lineno)d]%(reset)s %(message)s',
+                datefmt='%d-%m-%Y %H:%M:%S',
+                log_colors=colors,
+            )
         self.setFormatter(formatter)
 
+
 class FileHandler(logging.FileHandler):
-    def __init__(self, path, formatter=None):
-        super().__init__(filename=path)
-        if isinstance(formatter, type(None)):
-            raise TypeError('Logger formatter cannot be of type None')
+    """Represents a FileHandler.
+    Subclasses logging.StreamHandler
+    Logs to a file, defaults to a predefined formatter
+    """
+
+    def __init__(self, path, formatter: Optional[Any] = None) -> None:
+        super().__init__(filename=path, encoding='utf-8')
+        if formatter is None:
+            formatter = logging.Formatter(
+                '%(asctime)s %(name)s %(levelname)-4s [%(filename)s:%(lineno)d] %(message)s', datefmt='%d-%m-%Y %H:%M:%S'
+            )
         self.setFormatter(formatter)
