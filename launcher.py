0f2eebc93d208920966a7a950682b796184ab8e7
theSystemFall <matheus.lougen@gmail.com>
2023-04-15 16:33:15 -0300

    refactor: migrate to .ini config files

--- launcher.py
+++ launcher.py
@@ -3,48 +3,53 @@
 import asyncio
 from typing import NoReturn
 
+import configparser
+from discord.ext import commands
+
 import systembot
 from systembot.modules import logger
-from systembot.modules import Config
 
-
-def setup_logging() -> None:
-    """Setup discord and wavelink loggers"""
-    logger.create('wavelink',
-                  level=logger.WARNING,
-                  handlers=[logger.StreamHandler(),
-                            logger.FileHandler('./data/logs/wavelink.log')])
-    logger.create('wavelink.websocket',
-                  level=logger.WARNING,
-                  handlers=[logger.StreamHandler(),
-                            logger.FileHandler('./data/logs/wavelink.log')])
-    logger.create('discord',
-                  level=logger.WARNING,
-                  handlers=[logger.StreamHandler(),
-                            logger.FileHandler('./data/logs/discord.log')])
-    logger.create('discord.https',
-                  level=logger.WARNING,
-                  handlers=[logger.StreamHandler(),
-                            logger.FileHandler('./data/logs/discord.https.log')])
+log = logger.create(
+    __name__, level=logger.DEBUG, handlers=[logger.StreamHandler(), logger.FileHandler(f'./data/logs/{__name__}.log')]
+)
 
 
 def main() -> None:
+    """Main function"""
+    logger.create(
+        'wavelink', level=logger.WARNING, handlers=[logger.StreamHandler(), logger.FileHandler('./data/logs/wavelink.log')]
+    )
+    logger.create(
+        'wavelink.websocket',
+        level=logger.WARNING,
+        handlers=[logger.StreamHandler(), logger.FileHandler('./data/logs/wavelink.log')],
+    )
+    logger.create(
+        'discord', level=logger.WARNING, handlers=[logger.StreamHandler(), logger.FileHandler('./data/logs/discord.log')]
+    )
+    logger.create(
+        'discord.https',
+        level=logger.WARNING,
+        handlers=[logger.StreamHandler(), logger.FileHandler('./data/logs/discord.https.log')],
+    )
+
     initial_extensions = (
         'commands.developer_commands',
         'commands.user_commands',
         'music.commands',
         'music.events',
         'music.exceptions',
-        )
+    )
 
-    setup_logging()
-    client = systembot.Client(config=Config(path='./data/config.yaml'), initial_extensions=initial_extensions)
+    config = configparser.ConfigParser()
+    config.read('./data/config.ini')
+    client = systembot.Client(config, initial_extensions)
     asyncio.run(run_bot(client))
 
 
-async def run_bot(client: systembot.Client) -> NoReturn:
+async def run_bot(client: commands.Bot) -> NoReturn:
     async with client:
-        await client.start(client.config.token)
+        await client.start(client.config['discord']['token'])
 
 
 if __name__ == '__main__':
